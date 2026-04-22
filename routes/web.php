9c8f55e0e0332578b07132383a9f2b28b9d2c30f
Cameron Rackley <cjrackley@uncg.edu>
2026-04-22 14:20:00 -0400

    fix for cart

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 
 Route::get('/cart', [CartController::class, 'index']);
-Route::get('/cart/add/{product}', [CartController::class, 'add']);
-Route::get('/cart/update/{product}', [CartController::class, 'update']);
-Route::get('/cart/remove/{product}', [CartController::class, 'remove']);
+Route::post('/cart/add/{product}', [CartController::class, 'add']);
+Route::post('/cart/update/{product}', [CartController::class, 'update']);
+Route::post('/cart/remove/{product}', [CartController::class, 'remove']);
 
 Route::get('/orders', [OrderController::class, 'index']);
-Route::get('/checkout', [OrderController::class, 'store']);
+Route::post('/checkout', [OrderController::class, 'store']);
